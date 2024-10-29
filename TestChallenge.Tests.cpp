887ad1a511f46736226097d002922dc49eebbf0c
Brundhap20 <96507337+Brundhap20@users.noreply.github.com>
2024-10-29 12:00:39 +0530

    Update TestChallenge.Tests.cpp

--- TestChallenge.Tests.cpp
+++ TestChallenge.Tests.cpp
@@ -1,9 +1,15 @@
 #include "TestChallenge.h"
 #include <gtest/gtest.h>
 
+class TestChallengeTest : public ::testing::Test {
+protected:
+    int result;
+    void SetUp() override {
+        result = 0;
+    }
+};
 
 TEST(TestChallengeTest, AddTwoPositiveNumbers) {
-    int result;
     add(3, 5, &result);
     EXPECT_EQ(result, 8);
 }
