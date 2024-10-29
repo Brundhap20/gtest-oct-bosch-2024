bfccd0cb65b6fee252e4e2d822607b01479326af
Brundhap20 <96507337+Brundhap20@users.noreply.github.com>
2024-10-29 10:58:18 +0530

    Update StringCalculatorValueParameter.Tests.cpp

--- StringCalculatorValueParameter.Tests.cpp
+++ StringCalculatorValueParameter.Tests.cpp
@@ -23,6 +23,14 @@ INSTANTIATE_TEST_SUITE_P(ValidValuesDataSet,StringCalculatorAddParameterized,tes
   make_tuple("",0),make_tuple("0",0),make_tuple("1",1),make_tuple("1,2",3),make_tuple("1,2,3",6),make_tuple("1\n2,3",6),make_tuple("//;\n1;2",3),make_tuple("42,1001,3",45),make_tuple("//[***]\n8***2***3",13),make_tuple("//[*][%]\n4*2%3",9),make_tuple("1,2",3),make_tuple("//[**][%^]\n4**1%^9",14)
 ));
 
+//For valid inputs 
+TEST_F(StringCalculatorAddParameterized, DataDrivenTestCase){
+    string input=std::get<0>(GetParam());
+    int expectedVal=std::get<1>(GetParam());
+    int actualVal=objUnderTest->Add(input);
+    ASSERT_EQ(actualVal,expectedVal);
+}
+
 //For invalid input 
 TEST_F(StringCalculatorAddFixture, when_passed_negative_numbers_throws_an_exception_listing_invalid_values){
     string input="1,-2, 3 -4";
