84a867079e49ac942090fd167d3cda28312651af
Brundhap20 <96507337+Brundhap20@users.noreply.github.com>
2024-10-29 11:48:28 +0530

    Update TestChallenge.Tests.cpp

--- TestChallenge.Tests.cpp
+++ TestChallenge.Tests.cpp
@@ -1,1 +1,9 @@
+#include "TestChallenge.h"
+#include <gtest/gtest.h>
+
+TEST(TestChallengeTest, CorrectSum) {
+    int x = 3 , y = 5;
+    int result= add(x, y, &result);
+    EXPECT_EQ(result, 8);
+}
 
